8918a9b40f5a66d096c2fce7457429474f63c58b
Emmanuel C. Cañete <canete.emmanuelc@gmail.com>
2026-02-09 22:09:23 +0800

    FEAT: api endpoint and rate limiting

---
+++ src/pages/api/[...path].ts
@@ -0,0 +1,48 @@
+export const prerender = false;
+
+import { retrieveFile } from '../../../index.js'; 
+import { LRUCache } from 'lru-cache';
+
+const tokenCache = new LRUCache({
+    max: 500,
+    ttl: 60 * 1000, 
+});
+
+export async function GET({ params, request }) {
+    const ip = request.headers.get('x-forwarded-for') || 'anonymous';
+    const tokenCount = (tokenCache.get(ip) as number) || 0;
+  
+    if (tokenCount >= 10) {
+        return new Response(JSON.stringify({ error: "Too many requests. Chill muna!" }), {
+            status: 429, 
+            headers: { "Content-Type": "application/json" }
+        });
+    }
+    tokenCache.set(ip, tokenCount + 1);
+    // ---------------------------
+
+    const fullPath = params.path;
+
+    if (!fullPath) {
+        return new Response(JSON.stringify({ error: "Path is required" }), { status: 400 });
+    }
+
+    const segments = fullPath.split('--');
+    const folderId = segments[segments.length - 1];
+
+    const currentUrl = fullPath;
+
+    try {
+        const data = await retrieveFile(folderId, currentUrl);
+
+        return new Response(JSON.stringify(data), {
+        status: 200,
+        headers: {
+            'Content-Type': 'application/json'
+        }
+        });
+    } catch (error) {
+        console.error("API Error:", error);
+        return new Response(JSON.stringify({ error: "Internal Server Error" }), { status: 500 });
+    }
+}
