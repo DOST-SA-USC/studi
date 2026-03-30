38757e624c6cd17e727be824aeaeebca50b1411f
Ryan <ryan@Ryans-MacBook-Air.local>
2026-03-30 15:11:39 +0800

    style: adding updated frontpage ui

--- src/pages/directory/utils/directoryValidation.ts
+++ src/pages/directory/utils/directoryValidation.ts
@@ -25,7 +25,11 @@ const isDirectoryEntry = (value: unknown): value is DirectoryEntry => {
     return false;
   }
 
-  if (value.type === "file" && !isString(value.viewLink) && !isString(value.contentLink)) {
+  if (
+    value.type === "file" &&
+    !isString(value.viewLink) &&
+    !isString(value.contentLink)
+  ) {
     return false;
   }
 
